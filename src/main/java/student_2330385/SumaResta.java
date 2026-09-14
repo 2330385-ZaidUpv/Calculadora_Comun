a4691b69de9f8f707d5d7e2300f99ffeb8348c49
Fernando <2530095@upv.edu.mx>
2026-09-13 18:59:18 -0600

    Agrega lógica de suma y resta

--- src/main/java/student_2330385/SumaResta.java
+++ src/main/java/student_2330385/SumaResta.java
@@ -1,4 +1,11 @@
 package student_2330385;
 
 public class SumaResta {
+    public double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public double restar(double a, double b) {
+        return a - b;
+    }
 }
